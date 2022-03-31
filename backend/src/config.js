760e1dc34e8cd82b0f821caa9fad86cdf43d6452
Skyeeter <auswilder@gmail.com>
2022-03-30 22:27:36 -0400

    address

--- backend/src/config.js
+++ backend/src/config.js
@@ -70,7 +70,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xB7fc2fB7D1A936E673Ee0C497384981B0EbC30cD"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
